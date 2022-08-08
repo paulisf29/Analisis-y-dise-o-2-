e28adb2d71768feb8912a8704b4645dacc0ff7ff
puqui29 <106561888+puqui29@users.noreply.github.com>
2022-08-08 14:35:13 -0600

    codigo base

---
+++ administrador_de_tareas/src/main/java/com/mycompany/administrador_de_tareas/Administrador_de_tareas.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.administrador_de_tareas;
+
+/**
+ *
+ * @author Paulina Fallas
+ */
+public class Administrador_de_tareas {
+
+    public static void main(String[] args) {
+        new pantalla_Principal().setVisible(true);
+    }
+}
